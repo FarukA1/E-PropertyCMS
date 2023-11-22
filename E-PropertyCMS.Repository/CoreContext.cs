408d2c554c2eaa2c62da8ae1a41401ef44e79239
farukawofeso <farukawofeso@outlook.com>
2023-11-22 17:51:21 +0000

    Setting the modelBuilder

--- E-PropertyCMS.Repository/CoreContext.cs
+++ E-PropertyCMS.Repository/CoreContext.cs
@@ -52,5 +52,56 @@ public class CoreContext : DbContext, ICoreContext
         }
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ClientDbModel>().HasKey(t => t.Id);
+        modelBuilder.Entity<ClientDbModel>().ToTable("Client");
+        modelBuilder.Entity<ClientDbModel>().Property(t => t.Title).HasMaxLength(250);
+        modelBuilder.Entity<ClientDbModel>().Property(t => t.FirstName).HasMaxLength(250);
+        modelBuilder.Entity<ClientDbModel>().Property(t => t.LastName).HasMaxLength(250);
+        modelBuilder.Entity<ClientDbModel>().Property(t => t.Email).HasMaxLength(50);
+        modelBuilder.Entity<ClientDbModel>().Property(t => t.Phone).HasMaxLength(20);
+        modelBuilder.Entity<ClientDbModel>().HasOne(t => t.Address).WithMany().HasForeignKey(v => v.AddressId);
+        modelBuilder.Entity<ClientDbModel>().HasMany(t => t.Properties).WithOne(v => v.PropertyOwner).HasForeignKey(property => property.PropertyOwnerId);
+        modelBuilder.Entity<ClientDbModel>().HasMany(t => t.Properties).WithOne(v => v.CurrentOccupant).HasForeignKey(property => property.CurrentOccupantId);
+
+        modelBuilder.Entity<AddressDbModel>().HasKey(t => t.Id);
+        modelBuilder.Entity<AddressDbModel>().ToTable("Address");
+        modelBuilder.Entity<AddressDbModel>().Property(t => t.Number).HasMaxLength(50);
+        modelBuilder.Entity<AddressDbModel>().Property(t => t.StreetName).HasMaxLength(50);
+        modelBuilder.Entity<AddressDbModel>().Property(t => t.City).HasMaxLength(50);
+        modelBuilder.Entity<AddressDbModel>().Property(t => t.County).HasMaxLength(50);
+        modelBuilder.Entity<AddressDbModel>().Property(t => t.PostCode).HasMaxLength(50);
+        modelBuilder.Entity<AddressDbModel>().Property(t => t.Country).HasMaxLength(50);
+
+        modelBuilder.Entity<PropertyDbModel>().HasKey(t => t.Id);
+        modelBuilder.Entity<PropertyDbModel>().ToTable("Property");
+        modelBuilder.Entity<PropertyDbModel>().Property(t => t.Price).HasMaxLength(50);
+        modelBuilder.Entity<PropertyDbModel>().Property(t => t.Description).HasMaxLength(1000);
+        modelBuilder.Entity<PropertyDbModel>().HasOne(t => t.Address).WithMany().HasForeignKey(v => v.AddressId);
+
+        modelBuilder.Entity<RoomDbModel>().HasKey(t => t.Id);
+        modelBuilder.Entity<RoomDbModel>().ToTable("Room");
+        modelBuilder.Entity<RoomDbModel>().Property(t => t.RoomType).HasMaxLength(50);
+        modelBuilder.Entity<RoomDbModel>().Property(t => t.Description).HasMaxLength(1000);
+
+        modelBuilder.Entity<CaseDbModel>().HasKey(t => t.Id);
+        modelBuilder.Entity<CaseDbModel>().ToTable("Case");
+        modelBuilder.Entity<CaseDbModel>().HasOne(t => t.Client).WithMany().HasForeignKey(v => v.ClientId);
+        modelBuilder.Entity<CaseDbModel>().HasOne(t => t.Property).WithMany().HasForeignKey(v => v.PropertyId);
+        modelBuilder.Entity<CaseDbModel>().HasOne(t => t.CaseType).WithMany().HasForeignKey(v => v.CaseTypeId);
+        modelBuilder.Entity<CaseDbModel>().HasOne(t => t.CaseStatus).WithMany().HasForeignKey(v => v.CaseStatusId);
+
+        modelBuilder.Entity<CaseTypeDbModel>().HasKey(t => t.Id);
+        modelBuilder.Entity<CaseTypeDbModel>().ToTable("CaseType");
+        modelBuilder.Entity<CaseTypeDbModel>().Property(t => t.Type).HasMaxLength(100);
+
+        modelBuilder.Entity<CaseStatusDbModel>().HasKey(t => t.Id);
+        modelBuilder.Entity<CaseStatusDbModel>().ToTable("CaseStatus");
+        modelBuilder.Entity<CaseStatusDbModel>().Property(t => t.Status).HasMaxLength(100);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
 }
 

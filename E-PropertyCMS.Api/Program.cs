758e063aeccc54e64bbc6314c462469ee673760e
farukawofeso <farukawofeso@outlook.com>
2023-12-06 19:55:06 +0000

    implement in-memory caching

--- E-PropertyCMS.Api/Program.cs
+++ E-PropertyCMS.Api/Program.cs
@@ -1,4 +1,5 @@
 ﻿using E_PropertyCMS.Core.Application.ConvertDtoToDomain;
+using E_PropertyCMS.Core.Caching;
 using E_PropertyCMS.Core.Repositories;
 using E_PropertyCMS.Core.Services;
 using E_PropertyCMS.Repository;

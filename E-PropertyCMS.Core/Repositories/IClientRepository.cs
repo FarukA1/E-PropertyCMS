656d179955e30a85b41745cc5dd3d1244c8dc083
farukawofeso <farukawofeso@outlook.com>
2023-12-07 23:29:07 +0000

    Clean up

--- E-PropertyCMS.Core/Repositories/IClientRepository.cs
+++ E-PropertyCMS.Core/Repositories/IClientRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using E_PropertyCMS.Domain.Enumeration;
 using E_PropertyCMS.Domain.Filter;
 using E_PropertyCMS.Domain.Model;
@@ -8,9 +9,6 @@ namespace E_PropertyCMS.Core.Repositories
 	public interface IClientRepository
 	{
         Task<List<Client>> GetClients();
-        Task<List<Client>> GetClients(PaginationFilter filter);
-        Task<List<Client>> GetClientsByType(ClientType? clientType);
-        Task<List<Client>> GetClientsByType(ClientType? clientType, PaginationFilter filter);
         Task<Client> GetClientById(Guid Id);
         Task<List<Property>> GetClientProperties(Guid clientId);
         Task<Client> StoreClient(Client client);

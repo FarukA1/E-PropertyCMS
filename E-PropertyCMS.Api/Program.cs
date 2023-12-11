274aab09e7f582a7e91610d3db2b4bc589547a91
farukawofeso <farukawofeso@outlook.com>
2023-12-11 10:38:50 +0000

    User and Account

--- E-PropertyCMS.Api/Program.cs
+++ E-PropertyCMS.Api/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Security.Claims;
+using Auth0.AspNetCore.Authentication;
+using Auth0.ManagementApi;
 using E_PropertyCMS.Api.Auth;
 using E_PropertyCMS.Core.Application.ConvertDtoToDomain;
 using E_PropertyCMS.Core.Caching;
@@ -9,6 +11,7 @@ using E_PropertyCMS.Repository.Contexts;
 using E_PropertyCMS.Repository.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
@@ -20,8 +23,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
 {
-    options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
-    options.Audience = builder.Configuration["Auth0:Audience"];
+    options.Authority = $"https://{builder.Configuration["Api-Auth0:Domain"]}/";
+    options.Audience = builder.Configuration["Api-Auth0:Audience"];
     options.TokenValidationParameters = new TokenValidationParameters
     {
         NameClaimType = ClaimTypes.NameIdentifier
@@ -34,10 +37,25 @@ builder.Services
       options.AddPolicy(
         "read:messages",
         policy => policy.Requirements.Add(
-          new HasScopeRequirement("read:messages", builder.Configuration["Auth0:Domain"])
+          new HasScopeRequirement("read:messages", builder.Configuration["Api-Auth0:Domain"])
         )
       );
   });
+
+//builder.Services.AddAuth0WebAppAuthentication(options =>
+//{
+//    options.Domain = builder.Configuration["Web-Auth0:Domain"];
+//    options.ClientId = builder.Configuration["Web-Auth0:ClientId"];
+//});
+
+builder.Services.AddAuth0AuthenticationClient(config =>
+{
+    config.Domain = builder.Configuration["AccountManagement-Auth0:Domain"];
+    config.ClientId = builder.Configuration["AccountManagement-Auth0:ClientId"];
+    config.ClientSecret = builder.Configuration["AccountManagement-Auth0:ClientSecret"];
+});
+
+builder.Services.AddAuth0ManagementClient().AddManagementAccessToken();
 
 // Add services to the container.
 
@@ -50,11 +68,13 @@ builder.Services.AddScoped<ICoreContext, CoreContext>();
 builder.Services.AddTransient<IClientRepository, ClientRepository>();
 builder.Services.AddTransient<ICaseRepository, CaseRepository>();
 builder.Services.AddTransient<IPropertyRepository, PropertyRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
 
 builder.Services.AddTransient<IDtoToDomain, DtoToDomain>();
 builder.Services.AddTransient<IClientService, ClientService>();
 builder.Services.AddTransient<ICaseService, CaseService>();
 builder.Services.AddTransient<IPropertyService, PropertyService>();
+builder.Services.AddTransient<IUserService, UserService>();
 
 builder.Services.AddDbContext<CoreContext>(opt => opt.UseNpgsql(
     builder.Configuration.GetConnectionString("DefaultConnection")
@@ -76,6 +96,20 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "E-PropertyCMS.Api", Version = "v1" });
 });
 
+var corsPolicy = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicy,
+        builder =>
+        {
+            builder.
+            AllowAnyOrigin().
+            AllowAnyMethod().
+            AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -86,6 +120,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseRouting();
+
+app.UseCors(corsPolicy);
 
 app.UseAuthentication();
 app.UseAuthorization();
---
+++ E-PropertyCMS.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using E_PropertyCMS.Domain.Model;
+
+namespace E_PropertyCMS.Core.Repositories
+{
+	public interface IUserRepository
+	{
+        Task<List<User>> GetUsers();
+        Task<User> GetUserById(Guid Id);
+        Task<User> StoreUser(User user);
+    }
+}
+
